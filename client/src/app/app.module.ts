0e0dcf53f2cfdd18a86fedcdfd56640ec59cf6b6
Zihao1319 <zihao1319@gmail.com>
2023-05-12 09:10:04 +0800

    first commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { View1Component } from './components/view1.component';
+import { View0Component } from './components/view0.component';
+import { View2Component } from './components/view2.component';
+
+@NgModule({
+  declarations: [AppComponent, View1Component, View0Component, View2Component],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
